d02e92207ea032de0d7d2fd1453c32ae1da9fde7
ThomasKenyeres <kenyerest97@gmail.com>
2017-02-05 16:16:50 +0100

    StoreManager methods skeleton

---
+++ src/StorageManager.java
@@ -0,0 +1,21 @@
+/**
+ * Created by k on 2017.02.04..
+ */
+public class StorageManager {
+    public void addStorage(StoreCapable storage) {
+    }
+
+    public void addCDProduct(String name, int price, int tracks) {
+    }
+
+    public void addBookProduct(String name, int price, int size) {
+    }
+
+    public String listProducts() {
+        return null;
+    }
+
+    public int getTotalProductPrice() {
+        return 0;
+    }
+}
